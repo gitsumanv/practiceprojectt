ac789deab5061a86ff5ee55863619074654c7e40
gitsumanv <sumanvh007@gmail.com>
2023-03-01 00:47:16 +0530

    removed one blank space in main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ rec = collection.insert_one(d)
 
 #Lets Verify all the record at once present in the record with all the fields
 all_record = collection.find()
-
 #Printing all records present in the collection
 for idx, record in enumerate(all_record):
      print(f"{idx}: {record}")
